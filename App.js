fd2a95753e409f079eda19d7b16bcc7b49c5147f
Ganesh Sontakke <98448709+GaneshSontakke@users.noreply.github.com>
2023-01-24 19:03:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CustomForm from "./components/Form";
+
+function App() {
+  return (
+    <div id="App">
+      <CustomForm />
+    </div>
+  );
+}
+
+export default App;
